cc55c10eaea47e5a6e8f0c0c4ce01cecf8144d19
Maitri A Shah <48393362+mshah97@users.noreply.github.com>
2020-01-21 18:33:23 -0600

    Add files via upload

---
+++ _tocelsius.js
@@ -0,0 +1,6 @@
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+}
+document.getElementById("resultArea").innerHTML = toCelsius(68);
+alert (toCelsius(68));
+// answer in celsius is 20
